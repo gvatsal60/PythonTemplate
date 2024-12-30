a8efa6c0a26e77c31768a772905ecfd0cf6398b0
Vatsal Gupta <gvatsal60@Vatsals-Air.domain.name>
2024-12-30 20:56:29 +0530

    Minor Build Fix

--- Makefiles/rules.mk
+++ Makefiles/rules.mk
@@ -1,9 +1,10 @@
 # Targets
 .PHONY: all test clean
 
-PYTHON_CMD := python3
-BUILD_TOOL_CMD := $(PYTHON_CMD) -m
+NPROC := $(shell nproc)
+
+BUILD_TOOL_CMD := make -j$(NPROC) -C $(TOP_DIR)
 BUILD_CMD := $(BUILD_TOOL_CMD) build
 TEST_CMD := $(BUILD_TOOL_CMD) test
 RUN_CMD := $(BUILD_TOOL_CMD) run
-CLEAN_CMD := rm -rf .mypy_cache/ .venv/
+CLEAN_CMD := $(BUILD_TOOL_CMD) clean
